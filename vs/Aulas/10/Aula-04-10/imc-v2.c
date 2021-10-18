3a4feea049cc9a4679588e7f859d3968cd07359c
Gabriel Couto <gabrielcouto3.14@gmail.com>
2021-10-18 17:10:35 -0300

    main commit

---
+++ vs/Aulas/10/Aula-04-10/imc-v2.c
@@ -0,0 +1,59 @@
+/*
+    O algoritmo apresenta o peso, a altura, o IMC e a interpretacao do IMC.
+    O usuario ira inserir o peso e a altura no lancamento do programa.
+    Por Gabriel Couto de Freitas em 2021
+*/
+
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h> 
+
+void calc_imc(float peso, float altura);
+int main(int argc,char *argv[])
+{
+    
+    if (argc>1){
+        if(argc<3){
+            fprintf(stderr,"\nFaltam argumentos.\nDigite o peso e a altura.\n");
+            return 3;
+        }
+        float p=0;
+        float a=0;
+        if (strcmp(argv[1],"-p")==0){
+            p=atof(argv[2]);
+            a=atof(argv[4]);
+        }
+        else {
+            p=atof(argv[4]);
+            a=atof(argv[2]);
+        }
+        
+        calc_imc(p,a);
+    }
+    else 
+        fprintf(stderr,"\nNao ha argumentos.\n");
+
+    return 0;  //Encerra o programa e retorna o valor 0
+}
+
+/*
+calc_imc
+--------------------
+A função calcula e interpreta o IMC com o peso e altura inseridos
+e diz se o usuario e abaixo do peso, peso normal, acima do peso ou obeso.
+
+*/
+void calc_imc(float peso, float altura)
+{
+    float imc=0;
+    imc=peso/(altura*altura);
+    printf("\nIMC=%.2f\n",imc);
+    if(imc<18.5)
+        printf("\nAbaixo do peso.\n\n");
+    else if(imc<25.0)
+        printf("\nIMC normal.\n\n");
+    else if(imc<30.0)
+        printf("\nSobrepeso.\n\n");
+    else   
+        printf("\nObesidade.\n\n");
+}
